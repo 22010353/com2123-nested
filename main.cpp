ccb24c962f87c86a0db522f47ba1514af30006e2
22010353 <163529321+22010353@users.noreply.github.com>
2024-04-18 20:52:41 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int partner;
+cout<<"Enter your number scale to get your desired partner:";
+cin>>partner;
+if(partner>0)
+{
+if(partner<0)
+{
+cout<<"Partner is skinny and beautiful if female and dark and handsome if male:";
+}
+else{
+cout<<"Partner is chubby and beautiful and lightskinned handsome if male :";
+}
+}
+else{
+cout<<"Partner is skinny and beautiful if female and dark and handsome:";
+}
+    return 0;
+}
